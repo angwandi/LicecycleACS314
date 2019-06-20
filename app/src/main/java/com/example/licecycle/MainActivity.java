cef1003a69b30b11d2949c2e92f35b99bd9e7008
Innocent Angwadia <demsdema217@gmail.com>
2019-06-20 15:14:16 +0300

    Add Design UI Components

--- app/src/main/java/com/example/licecycle/MainActivity.java
+++ app/src/main/java/com/example/licecycle/MainActivity.java
@@ -5,8 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.content.res.Resources;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -18,117 +16,17 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
     private Context context;
     private int duration = Toast.LENGTH_SHORT;
-    private Button btnExit;
-    private EditText txtColorSelected;
-    private TextView txtSpyBox;
     private LinearLayout myScreen;
-    private String PREFNAME = "myPrefFile1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txtColorSelected = findViewById(R.id.editext);
-        btnExit = findViewById(R.id.button);
-        txtSpyBox = findViewById(R.id.textView1);
-        myScreen = findViewById(R.id.myScreen1);
-        btnExit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
-        txtColorSelected.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                //nothing todo here
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                //nothing todo here as well
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                String chosenColor = s.toString().toLowerCase(Locale.US);
-                txtSpyBox.setText(chosenColor);
-                setBackgroundColor(chosenColor, myScreen);
-            }
-        });
         context = getApplicationContext();
-        Toast.makeText(context, "onCreate", duration).show();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(context, "onDestroy", duration).show();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        String chosenColor = txtSpyBox.getText().toString();
-        saveStateData(chosenColor);
-        Toast.makeText(context, "onPause", duration).show();
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Toast.makeText(context, "onRestart", duration).show();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Toast.makeText(context, "onResume", duration).show();
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        updateMeUsingSavedStateData();
-        Toast.makeText(context, "onStart", duration).show();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Toast.makeText(context, "onStop", duration).show();
-    }
-
-    private void setBackgroundColor(String chosenColor, LinearLayout myScreen) {
-        if (chosenColor.contains("red"))
-            myScreen.setBackgroundColor(0xffff0000);
-        if (chosenColor.contains("green"))
-            myScreen.setBackgroundColor(0xff00ff00);
-        if (chosenColor.contains("blue"))
-            myScreen.setBackgroundColor(0xff0000ff);
-        if (chosenColor.contains("white"))
-            myScreen.setBackgroundColor(0xffffffff);
-    }
-
-    private void updateMeUsingSavedStateData() {
-        SharedPreferences myPrefContainer = getSharedPreferences(PREFNAME, Activity.MODE_PRIVATE);
-        String key = "chosenBackgroundColor";
-        String defaultValue = "white";
-        if ((myPrefContainer != null) && myPrefContainer.contains(key)) {
-            String color = myPrefContainer.getString(key, defaultValue);
-            setBackgroundColor(color, myScreen);
-        }
-    }
-
-    private void saveStateData(String chosenColor) {
-        SharedPreferences myPrefContainer = getSharedPreferences(PREFNAME, Activity.MODE_PRIVATE);
-        SharedPreferences.Editor myPrefEditor = myPrefContainer.edit();
-        String key = "chosenBackgroundColor";
-        String value = txtSpyBox.getText().toString();
-        myPrefEditor.putString(key, value);
-        myPrefEditor.apply();
+        myScreen = findViewById(R.id.myScreen1);
     }
 }
